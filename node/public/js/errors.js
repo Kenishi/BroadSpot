d02fe092498266bb0c4f6227b33b72e0653e1257
Jeremy May <kenishi86@gmail.com>
2015-03-22 05:20:25 -0400

    Add new errors file
    Shared between node and browser (hopefully), easy way to bring error
    messages together in one place.

---
+++ node/public/js/errors.js
@@ -0,0 +1,29 @@
+var errors = {
+	INVALID_SESSION : function() {
+		this.code=1;
+		this.msg="Session is invalid, try logging in again.";
+	},
+	ACCESS_TOKEN_EXPIRED : function() {
+		this.code=2;
+		this.msg="Access tokens expired, try making the request again.";
+	},
+	ACCESS_ERROR : function() {
+		this.code=3;
+		this.msg="Error occured while accessing server. Server reply: ";
+	},
+	FETCH_PLAYLISTS_FAILED : function() {
+		this.code=4;
+		this.msg="Failed to get playlists. Reason: ";
+	},
+	ERROR_COPYING_PLAYLIST : function() {
+		this.code=5;
+		this.msg="Error attempting to copy over playlist. Response Data: ";
+	},
+	INVALID_PARAMETERS : function() {
+		this.code=6;
+		this.msg="Invalid paramteres supplied";
+	}
+}
+
+// For node.js
+module.exports = errors;
